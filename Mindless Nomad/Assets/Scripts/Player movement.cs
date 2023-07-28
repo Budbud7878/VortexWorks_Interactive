db4f448b4d892653b640e5b230e31bc6c1515dd8
Budbud7878 <borisramosdiaz@icloud.com>
2023-07-28 04:02:56 -0400

    Did some optimization and fixed jump

--- Mindless Nomad/Assets/Scripts/Player movement.cs
+++ Mindless Nomad/Assets/Scripts/Player movement.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
+    //Variables that can be changed to what ever value we need them too so we don't have to change 10 different lines of code.
     private float horizontal;
     private bool sprinting;
     private float speed = 6f;
+    private float crouchspeed = 3f;
+    private float speedmin = 6f;
+    private float speedmax = 12f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
     public SpriteRenderer sc;
     public Animator animator;
-    
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -24,35 +27,29 @@ public class Playermovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Checks for inputs and returns -1 0 or 1
         horizontal = Input.GetAxisRaw("Horizontal");
         sprinting = Input.GetKey(KeyCode.LeftShift);
 
+        //Makes the waslikng animation activate
         animator.SetFloat("speed", Mathf.Abs(horizontal));
 
-
+        //This makes the player jump
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
-
+        //This make the jumping animation turn on
         if (rb.velocity.y > 0f)
         {
             animator.SetBool("isjumping", true);
         }
-
+        //This one makes the jumping animation turn off
         if (rb.velocity.y == 0f)
         {
             animator.SetBool("isjumping", false);
         }
-
-        if (Input.GetButtonDown("Jump") == true)
-        {
-            animator.SetBool("isjumping", true);
-        }
-        if (IsGrounded() == true)
-        {
-            animator.SetBool("isjumping", false);
-        }
+        //This Makes it so the more you press space the more you'll jump
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
@@ -89,15 +86,17 @@ public class Playermovement : MonoBehaviour
     {
         if (sprinting == true)
         {
+            // This makes the player run
             speed = speed + 0.1f;
             SpeedLimit();
         }
         if (sprinting && Input.GetKey(KeyCode.C) == false)
         {
+            // This decreases the speed of the player to the normal speed.
             speed = speed - 0.0001f;
-            if (speed <= 6f)
+            if (speed <= speedmin)
             {
-                speed = 6f;
+                speed = speedmin;
             }
         }
     }
@@ -105,33 +104,39 @@ public class Playermovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.C) == true && IsGrounded())
         {
-            // HERE WE NEED TO ADD ADJUST FOR BOX COLIDER WHILE CROUCHING
+            // This makes the player crouch
+            // !!! IMPORTANT : HERE WE NEED TO ADJUST BOX COLIDER WHILE CROUCHING
             animator.SetBool("iscrouching", true);
-            speed = 4f;
+            speed = crouchspeed;
             if (Input.GetKey(KeyCode.LeftShift)){
-                speed = 4f;
+                speed = crouchspeed;
             }
+            
         }
         else
         {
+            //This make the crouhing animation stop
             animator.SetBool("iscrouching", false);
         }
     }
     private void SpeedLimit() {
-        if (speed >= 12)
+        //This limits the speed of the player so they don't go off flying like Uzain bolt.
+        if (speed >= speedmax)
         {
-            speed = 12;
+            speed = speedmax;
         }
     }
     private void LooseMomentum()
     {
+        //This make the player loose momentum when their not moving. It's for when their not moving and pressing shift they don't keep their max speed they revert back to normal speed slowly.
         if (horizontal == 0)
         {
-            speed = 6f;
+            speed = speedmin;
         }
     }
     private IEnumerator DelayS()
     {
+        //In case you ever need a delay Function.
         yield return new WaitForSeconds(1);
     }
 }
